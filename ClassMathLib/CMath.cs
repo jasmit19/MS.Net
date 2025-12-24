57d30392b5442ac0ba4901c667e21d8885cce98b
Jasmit Lonhare <jasmiiit19@gmail.com>
2025-12-24 20:37:29 +0530

    Add files via upload

---
+++ ClassMathLib/CMath.cs
@@ -0,0 +1,51 @@
+﻿namespace ClassMathLib
+{
+    public class CMath
+    {
+        // Public : available/ accessible within assembly and outside the assembly
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        // Private : accessible only with the same class/ module
+        private int Sub(int x, int y) 
+        { 
+            return x - y; 
+        }
+
+        // Protected : Available with the assembly and outside the assembly : if :
+        // accessible with the heirarchy of inheritace/ derived classes
+        protected int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        // Internal : only available with the same assembly
+        internal int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        // Protected - internal : only available with the same assembly and within the heirarchy
+        protected internal int Square(int x)
+        {
+            return x * x;
+        }
+    }
+
+
+    public class AdvMath : CMath        // within the same assembly
+    {
+        public void AdvWrappermethod()
+        {
+            base.Add(1, 2);         // public
+            //base.Sub(5, 4);       // PRIVATE method() not accessible out of the class
+            base.Mult(10, 5);       // protected 
+            base.Div(40, 8);        // internal - available only in same assembly
+            base.Square(7);         // protcted - internal
+
+            Console.WriteLine("Addition= {0}, Multiplication= {1}, Division= {2}, Square= {3}", base.Add(2, 3), base.Mult(10, 5), base.Div(40, 8), base.Square(7));
+        }
+    }
+}
---
+++ ClassMathLibraryClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using ClassMathLib;
+
+namespace ClassMathLibraryClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            AdvMath amath = new AdvMath();
+            amath.AdvWrappermethod();
+
+            MyMath mymath = new MyMath();
+            mymath.MyWrapperMethod();
+        }
+    }
+    public class MyMath : CMath
+    {
+        public void MyWrapperMethod()
+        {
+            base.Add(2, 3);     // PUBLIC
+            //base.Sub();       // PRIVATE method(), can't be accessed
+            base.Mult(2, 3);    // PROTECTED method(), available inside and outside the assembly if in Derived Class
+            //base.Div(2, 3);   // INTERNAL method(), available only in same assembly (not out)
+            base.Square(2);     // PROTECTED INTERNAL METHOD(), available bcoz we took thereference of parent assesmbly
+            
+            Console.WriteLine("Addition= {0}, Multiplication= {1}, Square= {2}", base.Add(2, 3), base.Mult(10, 5), base.Square(7));
+
+        }
+    }
+}
